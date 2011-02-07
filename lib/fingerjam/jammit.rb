5361e3c10bb6046096311da66d2aa4ba1f4e9127
Steve Hoeksema <steve@seven.net.nz>
2011-02-08 08:45:55 +1300

    Check for file existence before trying to md5sum

--- lib/fingerjam/jammit.rb
+++ lib/fingerjam/jammit.rb
@@ -3,7 +3,11 @@ module Jammit
 
     # Used by rake fingerjam:package
     def rewrite_asset_path(relative_path, absolute_path)
-      Fingerjam::Base.save_cached_url_from_path(relative_path, absolute_path)
+      if File.exist?(absolute_path)
+        Fingerjam::Base.save_cached_url_from_path(relative_path, absolute_path)
+      else
+        relative_path
+      end
     end
 
   end

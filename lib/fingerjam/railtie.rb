5cb9ae303935daa13cafc0d6eb1fc29f678f4e44
Steve Hoeksema <steve@seven.net.nz>
2011-02-21 10:49:37 +1300

    Configure in Railtie

--- lib/fingerjam/railtie.rb
+++ lib/fingerjam/railtie.rb
@@ -4,6 +4,8 @@ require "rails"
 module Fingerjam
   class Railtie < Rails::Railtie
     initializer "fingerjam.configure_rails_initialization" do
+      Fingerjam::Base.configure
+
       ActionView::Base.send(:include, Fingerjam::Helpers)
     end
   end

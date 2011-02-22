b0b120e70c7870e9707ee1f92552fa419d996712
Steve Hoeksema <steve@seven.net.nz>
2011-02-22 13:02:56 +1300

    cset copy cache

--- lib/fingerjam/capistrano/configuration.rb
+++ lib/fingerjam/capistrano/configuration.rb
@@ -1,8 +1,7 @@
 Capistrano::Configuration.instance(:must_exist).load do
 
   set :strategy, Capistrano::Deploy::Strategy::Fingerjam.new(self)
-  set :copy_cache, true
-
+  _cset(:copy_cache) { true }
   _cset(:cache_host) { "www.example.com" }
   _cset(:cache_protocol) { "https" }
 
